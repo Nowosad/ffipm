f7364faff39fbaa98f8056ed134b0e8fb8e57749
Nowosad <tupiszakaczki@gmail.com>
2020-09-23 15:49:21 +0200

    improves trap model plot

--- R/trap_model_plot.R
+++ R/trap_model_plot.R
@@ -1,48 +1,50 @@
-#' Title
+#' Trap Model Plot
 #'
-#' @param x
-#' @param rasters
+#' @param x An output from [read_pd()]
+#' @param rasters An output from [create_raster_stack()]
 #'
-#' @return
+#' @return A ggplot object
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#'   Path <- "C:/Google Drive/Corvus Geostat - Jakub Nowosad/"
+#'   Path <- "../corvus_dynamic_outputs/"
+#'   input_file <- paste0(Path,"NetCDF/DxResults_Cc_2010-2015.nc")
+#'   AllYears <- extract_data_list(input_file, "Weekly Growth Index",
+#'                               years = 2000:2014)
+#'   rasters <- create_raster_stack(AllYears, years = 2000:2014)
+#'
+#'   maindir <- "../corvus_dynamic_outputs/Digitising FF trapping plots/"
+#'   filename <- "Bjelis_2007_1"; species <- "Cc"
+#'   x <- read_pd(filename, species, maindir)
+#'
+#'   trap_model_plot(x, rasters)
+#' }
 trap_model_plot <- function(x, rasters){
-  x$type <- attr(x, "ylab")
+  x$variable <- attr(x, "ylab")
   all_years <- sort(unique(lubridate::year(x$time)))
   x_lat <- unique(x$lat)
   x_lon <- unique(x$lon)
+  x <- x[c(1, 2, 5, 3, 4)]
 
   x_model <- create_time_series(rasters,
                                 years = all_years,
                                 x = x_lon, y = x_lat)
 
   x_model <- dplyr::filter(x_model,
-                           date >= min(x$time),
-                           date <= max(x$time))
-  names(x_model) <- c("time", "value" )
+                           time >= min(x$time),
+                           time <= max(x$time))
+
   x_model$lat <- x_lat
   x_model$lon <- x_lon
-  x_model$type <- attr(rasters, "dname")
 
   all_x = rbind(x, x_model)
 
   ggplot2::ggplot(all_x) +
     ggplot2::geom_line(ggplot2::aes(time, value)) +
-    ggplot2::facet_wrap(~type, scales = "free_y") +
+    ggplot2::facet_wrap(~variable, scales = "free_y") +
     ggplot2::labs(title = attr(x, "location")) +
     theme_cg()
 }
 
-Path <- "./"
-input_file <- paste0(Path,"NetCDF/DxResults_Cc_2010-2015.nc")
-AllYears <- extract_data_list(input_file, "Weekly Growth Index",
-                              years = 2000:2014)
-
-rasters <- create_raster_stack(AllYears, years = 2000:2014)
-
-# filename <- "2002_Medfly_"; species <- "Cc"
-filename <- "Bjelis_2007_1"; species <- "Cc"
-x <- read_pd(filename, species)
-
-trap_model_plot(read_pd(5), rasters)
