228e7bf93f375df0b775aa7f709613cbbadfe0de
Nowosad <tupiszakaczki@gmail.com>
2020-09-23 13:41:07 +0200

    cleans extract data list

--- R/extract_data_list.R
+++ R/extract_data_list.R
@@ -1,15 +1,24 @@
-#' Title
+#' Extract Data List
 #'
-#' @param file
-#' @param dname
-#' @param n
-#' @param years
-#' @param ...
+#' @param file An ncdf file
+#' @param dname A variable name. One of "Weekly Growth Index", "Hot Stress", "Dry Stress", "Wet Stress", "Cold Stress", "Ecoclimatic Index", "Moisture Index", "Temperature Index"
+#' @param n A number of layers for each year. The default is 52
+#' @param years A vector indicating years in the input file
+#' @param ... Not used
 #'
-#' @return
+#' @return A nested list with spatio-thematic metadata.
+#'  Each nested list contains a data for each year
+#'  in a form of a set of matrices.
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#'   Path <- "C:/Google Drive/Corvus Geostat - Jakub Nowosad/"
+#'   Path <- "../corvus_dynamic_outputs/"
+#'   input_file <- paste0(Path,"NetCDF/DxResults_Cc_2010-2015.nc")
+#'   AllYears <- extract_data_list(input_file, "Weekly Growth Index",
+#'                               years = 2000:2014)
+#' }
 extract_data_list <- function(file, dname, n = 52, years, ...){
   climexncdf <- ncdf4::nc_open(file)
   lon <- ncdf4::ncvar_get(climexncdf, "Longitude")
@@ -34,9 +43,8 @@ extract_data_list <- function(file, dname, n = 52, years, ...){
   }
   result <- create_data_list(tmp_stack, n = n, years = years)
 
-  metadata <- list(lon = lon, lat = lat)
+  metadata <- list(lon = lon, lat = lat, dname = dname)
   attr(result, "metadata") <- metadata
-  attr(result, "dname") <- dname
 
   return(result)
 }
