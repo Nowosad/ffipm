6926a21bbe2dd40e5b308c9b7bc08cb6dd6e4522
[Karol Kozyra] <[karkoz3@st.amu.edu.pl]>
2021-11-25 00:34:57 +0100

    add create_composite_raster

---
+++ R/create_composite_raster.R
@@ -0,0 +1,72 @@
+#' Create Composite raster
+#'
+#'file1, file2, mask, years, indicatior
+#' @param file1 file An ncdf file for mask 0 values
+#' @param file2 file An ncdf file for mask 1 values
+#' @param mask Irrigation mask raster with values 0,1
+#' @param years A vector stating for which years the RasterStack should be created
+#' @param dname A variable name. One of "Weekly Growth Index", "Hot Stress", "Dry Stress", "Wet Stress", "Cold Stress", "Ecoclimatic Index", "Moisture Index", "Temperature Index"
+#'
+#' @return A Composite RasterStack with some metadata
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' composite_raster_dorsalis <- fun1(file1 = "WeeklyGI_B,dorsalis_2018-2019_World.nc",
+#'                                   file2 = "WeeklyGI_B,dorsalis_2018-2019_World_irrigation_set.nc",
+#'                                   mask = "CM30_1995H_V2_gmia_v5_aei_h_classified10ha.tif",
+#'                                   years = 2018:2019,
+#'                                   dname = "Weekly Growth Index")
+#'
+#' }
+create_composite_raster <- function(file1, file2, mask, years, dname) {
+  mask <- raster::raster(mask) # read mask
+
+  # extracts selected variable from the model (.nc) into a list of matrices
+  AllYears1 <- extract_data_list(file1, dname,
+                                 years = years)
+  # creates a RasterStack based on the result from extract_data_list()
+  rasters1 <- create_raster_stack(AllYears1, years = years)
+
+  # extracts selected variable from the model (.nc) into a list of matrices
+  AllYears2 <- extract_data_list(file2, dname,
+                                 years = years)
+  # creates a RasterStack based on the result from extract_data_list()
+  rasters2 <- create_raster_stack(AllYears2, years = years)
+  #raster::extent(rasters1)
+  raster_extent <- raster::extent(rasters2)
+  #raster::extent(mask)
+
+  mask_cropped <- raster::crop(mask, raster_extent)
+  # raster::extent(mask_cropped) == raster_extent
+  # raster::plot(mask_cropped)
+
+  # Create a new raster object that has the same values as x, except for the cells that are equal to maskvalue
+  new_file1 <- raster::mask(rasters1, mask = mask_cropped,
+                            maskvalue=1, updatevalue=NA)
+  new_file2 <- raster::mask(rasters2, mask = mask_cropped,
+                            maskvalue=0, updatevalue=NA)
+  # final_raster <- raster::cover(new_file1, new_file2)
+  # final_raster_stack <- raster::stack(final_raster)
+
+  #system.time({
+  # insert values from file2 to file1 where are nas
+  raster::values(new_file1) <- ifelse(is.na(raster::values(new_file1)), raster::values(new_file2), raster::values(new_file1))
+  composite_raster <- raster::stack(new_file1)
+
+  #})
+  # testRaster_composite1 <- subset(new_file1_stack, 1)
+  # writeRaster(testRaster_composite1, "testRaster_composite1.tif", overwrite=TRUE)
+
+  # both metod give the same values but second method is faster 20 sec vs 5 sec
+  #table(values(final_raster_stack) == values(composite_raster))
+
+  rm(AllYears1)
+  rm(AllYears2)
+  rm(rasters1)
+  rm(rasters2)
+  rm(new_file1)
+  rm(new_file2)
+
+  return(composite_raster)
+}
